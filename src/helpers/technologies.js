f565ab4131c051770f9a19c45e9b4479b437e23a
Ivan Navas <ivanrng1502@gmail.com>
2023-08-17 23:00:55 -0500

    first commit

---
+++ src/helpers/technologies.js
@@ -0,0 +1,20 @@
+import html from '../images/HTML5.png' 
+import css from '../images/CSS3.png'    
+import js from '../images/JS.png'    
+import ts from '../images/ts.png'    
+import react from '../images/react.png'    
+import sql from '../images/sql.png'    
+import mongo from '../images/mongodb.png'    
+import git from '../images/git.png'    
+
+
+export const technologies = [
+  {imagen: html, name: "HTML" },
+  {imagen: css, name: "CSS" },
+  {imagen: js, name: "JavaScript" },
+  {imagen: react, name: "React" },
+  {imagen: sql, name: "MySql" },
+  {imagen: mongo, name: "MongoDB" },
+  {imagen: ts, name: "TypeScript" },
+  {imagen: git, name: "Git" },
+]
