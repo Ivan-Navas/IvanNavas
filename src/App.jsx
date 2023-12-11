0db5064f9b11ece193da05dda945f12ee72e7c29
Iván Navas <ivanrng.work@gmail.com>
2023-12-11 09:36:27 -0500

    prueba hola mundo

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,3 @@
-import "./App.css";
-import MyInfo from "./components/MyInfo";
-import Technologies from "./components/Technologies";
-import Works from "./components/Works";
-import { ContextProvider } from "./Context/Context";
-import { myInfo } from "./helpers/myInfo";
-import myCharacter from "./images/myCharacter.jpeg";
-import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
 
 function App() {
   return (
