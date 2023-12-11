38678bb86a2d34c0c862a58daed10dd4733ced38
Iván Navas <ivanrng.work@gmail.com>
2023-12-11 09:29:51 -0500

    hola mundo

--- src/App.jsx
+++ src/App.jsx
@@ -9,38 +9,9 @@ import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
 
 function App() {
   return (
-    <>
-      <ContextProvider>
-        <div className="iconsContainer">
-          <a href="https://github.com/Ivan-Navas" target="_blank">
-            <AiFillGithub className="logos github" />
-          </a>
-          <a
-            href="https://www.linkedin.com/in/ivan-navas-0990021a6/"
-            target="_blank"
-          >
-            <AiFillLinkedin className="logos linkedin" />
-          </a>
-        </div>
-        <div className="myInfoContainer">
-          <MyInfo />
-          <div className="characterContainer">
-            <img className="character" src={myCharacter} alt="Character" />
-          </div>
-        </div>
-        <div className="mySecoContainer">
-          <div>
-            <h2 className="title margin">Tecnologías</h2>
-            <Technologies />
-          </div>
-          <p className="secundary">{myInfo.secundaryInfo}</p>
-        </div>
-        <div className="worksContainer">
-          <h2 className="title margin">Proyectos</h2>
-          <Works />
-        </div>
-      </ContextProvider>
-    </>
+    <div>
+        <h2>Hola mundo</h2>
+    </div>
   );
 }
 
